3c4a4ec18a49537cc79736193d3ac1f1435c0758
EmilyChagas <emily.chagas@fatec.sp.gov.br>
2025-02-12 17:27:00 -0300

    learn(ap01 - js): encerra a secao 1.1
    - vimos var, let e const

---
+++ Intro_JS/script.js
@@ -0,0 +1,20 @@
+//declarando constantes
+//const nome = 'Jose'
+//console.log(nome) 
+//nome = 'Pedro'
+//console.log(nome)
+
+//Declarando variaveis
+//var b = 2
+//let a = 2
+//console.log(b)
+//a = 3
+//console.log(a)
+
+//Pq não usar var
+//var linguagem = 'JS'
+//console.log("Aprendendo " + linguagem)
+//var linguagem = 'Java'
+//console.log("Aprendendo " + linguagem)
+// Var permite que duas variaveis tenha o mesmo nome, o que pode dar comflito
+// Var não se restrige a bloco de condicionais, ou seja uma varival var declarada num if pode aparecer fora dele
